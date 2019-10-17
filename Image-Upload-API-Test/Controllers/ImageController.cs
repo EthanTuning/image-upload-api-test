59399a8419d91cf66b0dc0f82fd8f18bd8ed62ff
Ethan Tuning <etuning5@hotmail.com>
2019-10-17 12:18:49 -0700

    Got everything working.. yay

--- Image-Upload-API-Test/Controllers/ImageController.cs
+++ Image-Upload-API-Test/Controllers/ImageController.cs
@@ -1,10 +1,43 @@
-﻿using System.Web.Http;
+﻿using Image_Upload_API_Test.Models;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace Image_Upload_API_Test.Controllers {
 
     public class ImageController : ApiController {
 
+        [Route("api/image")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> PostImage() {
 
+            HttpResponseMessage response;
+
+            if(!Request.Content.IsMimeMultipartContent()) {
+
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, "Unsupported request type. Must have a file.");
+
+            }
+
+            try {
+
+                MultipartStreamProvider provider = new BlobStorageMultipartStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                response = Request.CreateResponse(HttpStatusCode.OK, "File uploaded successfully. See:" + provider.ToString());
+
+            } catch(Exception err) {
+
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, err.Message);
+
+            }
+
+            return response;
+
+        }
 
     }
 
---
+++ Image-Upload-API-Test/Models/BlobStorageMultipartStreamProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Image_Upload_API_Test.Models {
+
+    public class BlobStorageMultipartStreamProvider : MultipartStreamProvider {
+
+        public override Stream GetStream(HttpContent parent, HttpContentHeaders headers) {
+
+            Stream stream = null;
+            ContentDispositionHeaderValue contentDisposition = headers.ContentDisposition;
+
+            if(contentDisposition != null) {
+
+                if(!string.IsNullOrWhiteSpace(contentDisposition.FileName)) {
+
+                    string connectionString = ConfigurationManager.AppSettings["BlobConnectionString"];
+                    string container = ConfigurationManager.AppSettings["Container"];
+
+                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+                    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                    CloudBlobContainer blobContainer = blobClient.GetContainerReference(container);
+
+                    CloudBlockBlob blob = blobContainer.GetBlockBlobReference(contentDisposition.FileName);
+                    stream=blob.OpenWrite();
+
+                }
+
+            }
+
+            return stream;
+
+        }
+
+    }
+
+}
